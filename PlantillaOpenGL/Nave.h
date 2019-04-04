ce4b50f3812912ca3623d596308d6743c3f41934
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-04-04 08:54:15 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaOpenGL/Nave.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Modelo.h"
+#include "glm/glm.hpp"
+#include "glm/gtx/transform.hpp"
+
+class Nave : public Modelo {
+public:
+	Nave();
+	enum Direccion {Izquierda, Derecha};
+	vec3 coordenadas;
+	float angulo;
+	float velocidadAngular = 0.2f;
+	float velocidad = 0.1f;
+	
+	void rotar(Direccion direccion);
+	void avanzar();
+	void actualizarMatrizTransformacion();
+};
